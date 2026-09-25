7f0baeaadb0a99ce9903d5035b4ca608444d9dcb
KZNowhere <supavich.032547@gmail.com>
2026-09-25 09:24:46 +0700

    chore: allow imperative R3F frame mutations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,11 @@ import nextTs from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["src/components/SceneCanvas.tsx"],
+    rules: {
+      "react-hooks/immutability": "off",
+    },
+  },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
